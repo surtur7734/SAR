2393e23e9b3f245da47f2904ac38418b72827c49
Lucifer <kevin_tabak@outlook.com>
2017-07-15 22:49:46 +0200

    Added a bootstrap site for testing in steads of the ugly one justin made.

---
+++ Site2/Home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!-- 
+    Created on : 15-jul-2017, 14:47:04
+    Author     : Kevin
+    Version    : 0.003
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="MainStyle.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <title>Home</title>
+    </head>
+    <body>
+        <div class="site-wrapper">
+            <nav id="NavBarID" class="navbar navbar-inverse">
+                <div class="container">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="Home.php">SAR</a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="Home.php">Home</a></li>
+                            <li><a href="about.php">About</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="jumbotron">
+            <div class="container"></div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h2>Argentina</h2>
+                    <p>
+                        The Argentine army was highly Germanophile; this influence had grown since 1904 and predated both world wars. It did not involve a rejection of democracy but rather an admiration of German military history. This admiration, combined with an intense Argentine nationalism, influenced the main stance of the army towards the war: to stay neutral. The arguments in favor ranged from Argentine military tradition (Argentina was neutral during World War I and the War of the Pacific), to the perception of the war as a conflict between foreign countries with no Argentine interests at stake, to Anglophobia, to rejection of foreign attempts to force Argentina into joining the war. Only a handful of military leaders actually supported Adolf Hitler.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
